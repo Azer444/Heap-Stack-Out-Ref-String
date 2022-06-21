b9f48582e108064c0b2ac8b946cd24e3e98e32e1
Azer444 <azerrh@code.edu.az>
2022-06-21 16:18:22 +0400

    Heap,Stack,Ref,Out,String

---
+++ Heap,Stack,Ref,Out,String/Program.cs
@@ -0,0 +1,33 @@
+﻿//Adlari Tersine Cevirmek
+
+
+using System;
+
+namespace Heap_Stack_Ref_Out_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdlariTersineCevir();
+        }
+        static void AdlariTersineCevir()
+        {
+            string name;
+            Console.Write("Add Name:");
+            name = Console.ReadLine();
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.Write(name[name.Length - i - 1]);
+            }
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
+
+
+
+
